2ba29a7e8bb015f7771fcd65dddf25ea4f5515c2
Matthieu Tricoire <76185622+MatthieuTricoire@users.noreply.github.com>
2024-04-11 16:53:56 +0100

    add metadata

--- src/app/(posts)/categorie/[slug]/page.tsx
+++ src/app/(posts)/categorie/[slug]/page.tsx
@@ -1,12 +1,37 @@
 import React from "react";
-import { getCategoryWithPosts } from "../../../../../sanity/lib/queries";
+import {
+  getCategoryWithPosts,
+  getCategoryWithPostsQuery,
+} from "../../../../../sanity/lib/queries";
 import { PostCard } from "@/components/posts/post-card";
+import { sanityFetch } from "../../../../../sanity/lib/client";
+import { Metadata } from "next";
+import { Category } from "@/types/category";
 
 type SlugPageProps = {
   params: {
     slug: string;
   };
 };
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata> {
+  const slug = params.slug;
+
+  const categoryWithPosts: Category[] = await sanityFetch({
+    query: getCategoryWithPostsQuery,
+    qParams: { categorySlug: slug },
+    tags: ["post", "category"],
+  });
+  console.log("cate", categoryWithPosts);
+
+  return {
+    description: `Liste des projets pour la catégorie : ${categoryWithPosts.at(0)?.title} `,
+  };
+}
 
 const SlugPage = async ({ params }: SlugPageProps) => {
   const categoryWithPosts = await getCategoryWithPosts(params.slug);
