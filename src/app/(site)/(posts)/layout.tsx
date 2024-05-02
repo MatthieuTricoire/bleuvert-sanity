95844cb412200aa197ed0ee6e75c6256548b59f2
Matthieu Tricoire <matthieutricoire@Matthieus-MacBook-Pro.local>
2024-05-02 09:51:15 +0100

    change margin top

--- src/app/(site)/(posts)/layout.tsx
+++ src/app/(site)/(posts)/layout.tsx
@@ -8,7 +8,7 @@ const PostsLayout = async ({ children }: PostLayoutProps) => {
 
   return (
     <div className=" h-full flex-1 w-full flex flex-col  gap-10">
-      <h1 className="text-center mt-8 md:mt-10 text-3xl md:text-5xl font-[500]">
+      <h1 className="text-center mt-8 md:mt-24 text-3xl md:text-5xl font-[500]">
         Nos Réalisations
       </h1>
 
