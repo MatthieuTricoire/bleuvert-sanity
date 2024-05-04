adb651343ddc7be8c982b2bcd84e0db5c99b8bdc
Matthieu Tricoire <matthieutricoire@Matthieus-MacBook-Pro.local>
2024-05-04 15:38:51 +0100

    add condition on localisation and area

--- src/app/(site)/projet/[slug]/components/aside.tsx
+++ src/app/(site)/projet/[slug]/components/aside.tsx
@@ -9,16 +9,20 @@ type AsideProps = {
 export const Aside = ({ localisation, area }: AsideProps) => {
   return (
     <div className="self-center md:self-start flex md:flex-col  gap-4 text-gray-600">
-      <div className="flex">
-        <MapPin className="mr-2" /> {localisation}
-      </div>
-      <Separator className="hidden md:block" />
-      <div className="flex">
-        <Grid3X3 className="mr-2" />
-        <span>
-          {area}m<sup>2</sup>
-        </span>
-      </div>
+      {localisation && (
+        <div className="flex">
+          <MapPin className="mr-2" /> {localisation}
+        </div>
+      )}
+      {area && localisation && <Separator className="hidden md:block" />}
+      {area && (
+        <div className="flex">
+          <Grid3X3 className="mr-2" />
+          <span>
+            {area}m<sup>2</sup>
+          </span>
+        </div>
+      )}
     </div>
   );
 };
