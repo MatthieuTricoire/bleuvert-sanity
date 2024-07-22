e282f3a523f24be793557bd88ff19f8fc59b621c
Matthieu <matthieutricoire@gmail.com>
2024-07-22 14:46:49 +0200

    fix &apos;

--- src/app/(site)/_components/sections/features-section.tsx
+++ src/app/(site)/_components/sections/features-section.tsx
@@ -11,7 +11,7 @@ export const FeatureSection = () => {
           Nos prestations
         </Typography>
 
-        <Typography variant="body" component="p" className="text-justify sm:text-center">Nous concevons et aménageons des espaces intérieurs en tenant compte de l'esthétique, de la fonctionnalité et du confort. Nous travaillons sur divers projets, allant de la rénovation de maisons à l'aménagement de bureaux ou de commerces. Les prestations peuvent inclure : le conseil en design avec proposition de style, de couleurs et de matériaux, l'aménagement de l'espace avec plans détaillés et modèles 3D pour visualiser le projet, la sélection de mobilier et de décorations comprenant le choix de meuble, d'éclairages et d'objets décoratifs et enfin la maîtrise d'oeuvre assurant la coordination avec d'autres professionnels comme les entrepreneurs et les artisans.</Typography>
+        <Typography variant="body" component="p" className="text-justify sm:text-center">Nous concevons et aménageons des espaces intérieurs en tenant compte de l&apos;esthétique, de la fonctionnalité et du confort. Nous travaillons sur divers projets, allant de la rénovation de maisons à l&apos;aménagement de bureaux ou de commerces. Les prestations peuvent inclure : le conseil en design avec proposition de style, de couleurs et de matériaux, l&apos;aménagement de l&apos;espace avec plans détaillés et modèles 3D pour visualiser le projet, la sélection de mobilier et de décorations comprenant le choix de meuble, d&apos;éclairages et d&apos;objets décoratifs et enfin la maîtrise d&apos;oeuvre assurant la coordination avec d&apos;autres professionnels comme les entrepreneurs et les artisans.</Typography>
 
         <Link className={buttonVariants()} href="/prestations">
           En savoir plus
